12c9a39eaea4510f33529a3abd3846633fe7e2da
Vitor Tomic <vitor.tomic@vicert.com>
2021-03-30 13:30:35 +0200

    chart implementation

--- src/components/Observation.js
+++ src/components/Observation.js
@@ -27,44 +27,38 @@ export const Observation = () => {
         if (client && !observations && !error) {
             fetchObservations();
         } else {
-            const bmiChart = new Chart(chartElement.current, {
-                type: 'bar',
-                data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
+            initializeChart('bmi', ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], [12, 19, 3, 5, 2, 3], 'idx')
         }
       });
+    
+    const initializeChart = (label, labels, data, unit) => {
+        new Chart(chartElement.current, {
+            type: 'bar',
+            data: {
+                labels: labels, 
+                datasets: [{
+                    label: label,
+                    data: data, 
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)', 
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                },
+                tooltips: {
+                    callbacks: {
+                        label: (item) => `${item.yLabel} ${unit}`,
+                    },
+                }
+            }
+        });
+    }
     
     return (
         <div>
@@ -75,7 +69,9 @@ export const Observation = () => {
                 ))}
              </ul>
             <br/>
-            <canvas ref={chartElement} width="400" height="400"></canvas>
+            <div style={{width:'400px',height:'400px'}}>
+                <canvas ref={chartElement} width="400" height="400"></canvas>
+            </div>
         </div>
     )
 }
